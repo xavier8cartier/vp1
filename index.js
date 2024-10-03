bc1a8f8546aa357e497fee3d88f2838bdb99d213
Ksaveri Petrov <xavier8cartier@users.noreply.github.com>
2024-10-03 13:54:55 +0300

    EJS view engine kasutusele voetud

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-const express = require ("express");
+const express = require("express");
+const dtEt = require("./dateTime");
 const app = express();
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 
-app.get("/", (req, res)=>{
-	res.send("express tootab");
+app.get("/", (req, res) => {
+  //res.send("express tootab");
+  res.render("index.ejs");
 });
 
-app.listen(5118)
+app.get("/timenow", (req, res) => {
+  const weekdayNow = dtEt.weekDayET();
+  const dateNow = dtEt.dateEt();
+  const timeNow = dtEt.timeEt();
+  res.render("timenow", { nowWD: weekdayNow, nowD: dateNow, nowT: timeNow });
+});
+
+app.listen(5118);
