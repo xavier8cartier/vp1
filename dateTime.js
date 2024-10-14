fd36cdda1e85a8e94b31bbb2611a9f8c9b6f548d
Ksaveri Petrov <xavier8cartier@users.noreply.github.com>
2024-10-14 12:06:25 +0300

    Andmebaasiga yhendatud

--- dateTime.js
+++ dateTime.js
@@ -1,45 +1,69 @@
-const weekdayNamesEt = ["puhapaev", "esmaspaev", "teisipaev", "kolmapaev", "neljapaev", "reede", "laupaev"];
-const monthNamesEt = ["jaanuar", "veebruar", "marts", "aprill", "mai", "juuni", "juuli", "august", "september", "oktoober", "november", "detsember"];
+const weekdayNamesEt = [
+  "puhapaev",
+  "esmaspaev",
+  "teisipaev",
+  "kolmapaev",
+  "neljapaev",
+  "reede",
+  "laupaev",
+];
+const monthNamesEt = [
+  "jaanuar",
+  "veebruar",
+  "marts",
+  "aprill",
+  "mai",
+  "juuni",
+  "juuli",
+  "august",
+  "september",
+  "oktoober",
+  "november",
+  "detsember",
+];
 
-const dateEt = function(){
-//function dateEt(){
-	//const monthNamesEt = ["jaanuar", "veebruar", "mÃ¤rts", "aprill", "mai", "juuni", "juuli", "august", "september", "oktoober", "november", "detsember"];
-	let timeNow = new Date();
-	//console.log("Praegu on: " + timeNow);
-	let dateNow = timeNow.getDate();
-	let monthNow = timeNow.getMonth();
-	let yearNow = timeNow.getFullYear();
-	//const monthNamesEt = ["jaanuar", "veebruar", "marts", "aprill", "mai", "juuni", "juuli", "august", "september", "oktoober", "november", "detsember"];
-	//console.log("Praegu on: " + dateNow + "." + (monthNow + 1) + "." + yearNow);
-	//console.log("Praegu on: " + dateNow + ". " + monthNamesEt[monthNow] + " " + yearNow);
-	//const monthNamesEt = ["jan", "veb", "mar", "apr", "mai", "jun", "jul", "aug", "sep", "okt", "nov", "det"]
-	let dateNowEt = dateNow + ". " + monthNamesEt[monthNow] + " " + yearNow;
-	return dateNowEt;
-}
- const weekDayET = function(){
-	let timeNow = new Date();
-	let dayNow = timeNow.getDay();
-	//const weekdayNamesEt = ["pÃ¼hapÃ¤ev", "esmaspÃ¤ev", "teisipÃ¤ev", "kolmapÃ¤ev", "neljapÃ¤ev", "reede", "laupÃ¤ev"];
-	return weekdayNamesEt[dayNow];
-}
- const timeFormattedET = function(){
-	let timeNow = new Date();
-	let hourNow = timeNow.getHours();
-	let minuteNow = timeNow.getMinutes();
-	let secondNow = timeNow.getSeconds();
-	return hourNow + ":" + minuteNow + ":" + secondNow;
-}
+const dateEt = function () {
+  //function dateEt(){
+  //const monthNamesEt = ["jaanuar", "veebruar", "mÃ¤rts", "aprill", "mai", "juuni", "juuli", "august", "september", "oktoober", "november", "detsember"];
+  let timeNow = new Date();
+  //console.log("Praegu on: " + timeNow);
+  let dateNow = timeNow.getDate();
+  let monthNow = timeNow.getMonth();
+  let yearNow = timeNow.getFullYear();
+  //const monthNamesEt = ["jaanuar", "veebruar", "marts", "aprill", "mai", "juuni", "juuli", "august", "september", "oktoober", "november", "detsember"];
+  //console.log("Praegu on: " + dateNow + "." + (monthNow + 1) + "." + yearNow);
+  //console.log("Praegu on: " + dateNow + ". " + monthNamesEt[monthNow] + " " + yearNow);
+  //const monthNamesEt = ["jan", "veb", "mar", "apr", "mai", "jun", "jul", "aug", "sep", "okt", "nov", "det"]
+  let dateNowEt = dateNow + ". " + monthNamesEt[monthNow] + " " + yearNow;
+  return dateNowEt;
+};
+const weekDayET = function () {
+  let timeNow = new Date();
+  let dayNow = timeNow.getDay();
+  //const weekdayNamesEt = ["pÃ¼hapÃ¤ev", "esmaspÃ¤ev", "teisipÃ¤ev", "kolmapÃ¤ev", "neljapÃ¤ev", "reede", "laupÃ¤ev"];
+  return weekdayNamesEt[dayNow];
+};
+const timeFormattedET = function () {
+  let timeNow = new Date();
+  let hourNow = timeNow.getHours();
+  let minuteNow = timeNow.getMinutes();
+  let secondNow = timeNow.getSeconds();
+  return hourNow + ":" + minuteNow + ":" + secondNow;
+};
 
-const partOfDay = function(){
-	let dayPart = "suvaline hetk";
-	let timeNow = new Date();
-	if(timeNow.getHours >= 8 && timeNow.getHours() < 16){
-		dayPart = "kooliaeg";
-	}
-	return dayPart;
-}
+const partOfDay = function () {
+  let dayPart = "suvaline hetk";
+  let timeNow = new Date();
+  if (timeNow.getHours >= 8 && timeNow.getHours() < 16) {
+    dayPart = "kooliaeg";
+  }
+  return dayPart;
+};
 
-module.exports = {monthsEt: monthNamesEt, weekdaysEt: weekdayNamesEt, dateEt: dateEt, weekDayET: weekDayET, timeEt: timeFormattedET};
-
-
-
+module.exports = {
+  monthsEt: monthNamesEt,
+  weekdaysEt: weekdayNamesEt,
+  dateEt: dateEt,
+  weekDayET: weekDayET,
+  timeEt: timeFormattedET,
+};
--- index.js
+++ index.js
@@ -2,12 +2,22 @@ const express = require("express");
 const dtEt = require("./dateTime");
 const app = express();
 const fs = require("fs");
+const dbInfo = require("../../vp2024config");
+const mysql = require("mysql2");
 //paringu lahtiharutamiseks POST paringute puhul
 const bodyparser = require("body-parser");
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyparser.urlencoded({ extended: false }));
+
+//loon andmedbaasi yhenduse
+const conn = mysql.createConnection({
+  host: dbInfo.configData.host,
+  user: dbInfo.configData.user,
+  password: dbInfo.configData.passWord,
+  database: dbInfo.configData.dataBase,
+});
 
 app.get("/", (req, res) => {
   //res.send("express tootab");
@@ -41,25 +51,107 @@ app.get("/regvisit", (req, res) => {
   res.render("regvisit");
 });
 
+app.get("/visitlog", (req, res) => {
+  fs.readFile("public/textfiles/visitlog.txt", "utf8", (err, data) => {
+    if (err) {
+      res.render("justlist", {
+        h2: "Kylastuste logid",
+        listData: ["Ei leidnud yhtegi kylastust!"],
+      });
+    } else {
+      const visitLog = data
+        .trim()
+        .split("\n")
+        .map((entry, index) => `${index + 1}. ${entry}`);
+      res.render("justlist", { h2: "Kylastuste logid", listData: visitLog });
+    }
+  });
+});
+
 app.post("/regvisit", (req, res) => {
   console.log(req.body);
+
+  const firstName = req.body.firstNameInput;
+  const lastName = req.body.lastNameInput;
+  const dateTime = `${dtEt.dateEt()} ${dtEt.timeEt()}`;
+  const logEntry = `${firstName} ${lastName}; ${dateTime}\n`;
+
   fs.open("public/textfiles/visitlog.txt", "a", (err, file) => {
     if (err) {
       throw err;
     } else {
-      fs.appendFile(
-        "public/textfiles/visitlog.txt",
-        req.body.firstNameInput + " " + req.body.lastNameInput + ";",
-        (err) => {
-          if (err) {
-            throw err;
-          } else {
-            console.log("Faili kirjutati");
-            res.render("regvisit");
-          }
+      fs.appendFile("public/textfiles/visitlog.txt", logEntry, (err) => {
+        if (err) {
+          throw err;
+        } else {
+          console.log("log added to txt file");
+          res.render("regvisit");
         }
-      );
+      });
     }
   });
 });
+
+app.get("/regvisitdb", (req, res) => {
+  let notice = "";
+  let firstName = "";
+  let lastName = "";
+  res.render("regvisitdb", {
+    notice: notice,
+    firstName: firstName,
+    lastName: lastName,
+  });
+});
+
+app.post("/regvisitdb", (req, res) => {
+  let notice = "";
+  let firstName = "";
+  let lastName = "";
+  if (!req.body.firstNameInput || !req.body.lastNameInput) {
+    firstName = req.body.firstNameInput;
+    lastName = req.body.lastNameInput;
+    notice = "Osa andmeid sisestamata!";
+    res.render("regvisitdb", {
+      notice: notice,
+      firstName: firstName,
+      lastName: lastName,
+    });
+  } else {
+    let sqlreq = "INSERT INTO visitlog (first_name, last_name) VALUES (?,?)";
+    conn.query(
+      sqlreq,
+      [req.body.firstNameInput, req.body.lastNameInput],
+      (err, sqlres) => {
+        if (err) {
+          throw err;
+        } else {
+          notice = "Kylastus registreeritud!";
+          res.render("regvisitdb", {
+            notice: notice,
+            firstName: firstName,
+            lastName: lastName,
+          });
+        }
+      }
+    );
+  }
+});
+
+app.get("/eestifilm", (req, res) => {
+  res.render("filmindex");
+});
+
+app.get("/eestifilm/tegelased", (req, res) => {
+  let sqlReq = "SELECT first_name, last_name, birth_date FROM person";
+  conn.query(sqlReq, (err, sqlres) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log(sqlres);
+      res.render("tegelased", { persons: sqlres });
+    }
+  });
+  //res.render("tegelased");
+});
+
 app.listen(5118);
