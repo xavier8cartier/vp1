89d0d2e9a4fa3c972a5509962a7c47dbc7bc79b7
Ksaveri Petrov <xavier8cartier@users.noreply.github.com>
2024-11-07 15:35:06 +0200

    Sisselogise kood tehtud

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.use(bodyparser.urlencoded({ extended: true }));
 const upload = multer({ dest: "./public/gallery/orig/" });
 //pildimanipulatsiooniks
 const sharp = require("sharp");
+const bcrypt = require("bcrypt");
 
 //loon andmedbaasi yhenduse
 const conn = mysql.createConnection({
@@ -36,10 +37,13 @@ app.get("/", (req, res) => {
     if (err) {
       throw err;
     } else {
-      const latestNews = sqlres[0] || null;
+      let date = new Date(sqlres[0].news_date);
+      date = date.toUTCString();
       res.render("index.ejs", {
         dateDifference: dateDifference,
-        latestNews: latestNews,
+        newsTitle: sqlres[0].news_title,
+        newsDesc: sqlres[0].news_text,
+        newsDate: date,
       });
     }
   });
@@ -194,6 +198,109 @@ app.get("/visitlogdb", (req, res) => {
   });
 });
 
+app.get("/signin", (req, res) => {
+  res.render("signin");
+});
+
+app.post("/signin", async (req, res) => {
+  let notice = "";
+  const semStartDate = new Date("2024-09-02");
+  const today = new Date();
+  const timeDifference = today - semStartDate;
+  const dateDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+  const sql = "SELECT * FROM news ORDER BY id DESC LIMIT 1";
+  let sqlRes;
+  conn.query(sql, [], (err, sqlRes) => {
+    if (err) {
+      notice = "ERR 500, proovi uuesti.";
+      console.log("Login failed. err: br1");
+      console.error(err);
+      return res.status(500).render("signin", {
+        dateDifference: dateDifference,
+        newsTitle: sqlRes[0].title,
+        newsDesc: sqlRes[0].text,
+        newsDate: date,
+        notice,
+      });
+    } else {
+      let date = new Date(sqlRes[0].date);
+      date = date.toUTCString();
+      const { emailInput, passwordInput } = req.body;
+
+      if (!emailInput || !passwordInput) {
+        notice = "Moned andmeid puuduvad.";
+        console.log("Login failed. err: br2");
+        return res.status(400).render("signin", {
+          dateDifference: dateDifference,
+          newsTitle: sqlRes[0].news_title,
+          newsDesc: sqlRes[0].news_text,
+          newsDate: date,
+          notice,
+        });
+      }
+
+      conn.execute(
+        "SELECT id, password FROM users WHERE email = ?",
+        [emailInput],
+        (err, result) => {
+          if (err) {
+            notice = "ERR 500, proovi uuesti.";
+            console.log("Login failed. err: br3");
+            console.error(err);
+            return res.status(500).render("signin", {
+              dateDifference: dateDifference,
+              newsTitle: sqlRes[0].news_title,
+              newsDesc: sqlRes[0].news_text,
+              newsDate: date,
+              notice,
+            });
+          } else {
+            if (result[0] != null) {
+              bcrypt.compare(
+                passwordInput,
+                result[0].password,
+                (err, compRes) => {
+                  if (err) {
+                    notice = "Kasutajatunnus ja/voi parool on vale.";
+                    console.log("Login failed. err: incorrect password");
+                    return res.status(401).render("signin", {
+                      dateDifference: dateDifference,
+                      newsTitle: sqlRes[0].news_title,
+                      newsDesc: sqlRes[0].news_text,
+                      newsDate: date,
+                      notice,
+                    });
+                  } else {
+                    notice = "Olete sisse loginud!";
+                    console.log(`Login successful for user ${emailInput}`);
+                    return res.status(200).render("signin", {
+                      dateDifference: dateDifference,
+                      newsTitle: sqlRes[0].news_title,
+                      newsDesc: sqlRes[0].news_text,
+                      newsDate: date,
+                      notice,
+                    });
+                  }
+                }
+              );
+            } else {
+              notice = "Kasutajatunnus ja/voi parool on vale.";
+              console.log("Login failed. err: incorrect email");
+              return res.status(401).render("signin", {
+                dateDifference: dateDifference,
+                newsTitle: sqlRes[0].news_title,
+                newsDesc: sqlRes[0].news_text,
+                newsDate: date,
+                notice,
+              });
+            }
+          }
+        }
+      );
+    }
+  });
+});
+
 app.post("/", (req, res) => {
   if (req.body.personSubmit) {
     let sqlReq = "INSERT INTO person (first_name, last_name) VALUES (?, ?)";
@@ -306,39 +413,170 @@ app.post("/photoupload", upload.single("photoInput"), (req, res) => {
   //genereerime oma failinime
   const fileName = "vp_" + Date.now() + ".jpg";
   //nimetame uleslaetud failid umber
-  fs.rename(req.file.path, req.file.destination + fileName, (err) => {
-    console.log(err);
-  });
 
-  sharp(req.file.destination + fileName)
-    .resize(800, 600)
-    .jpeg({ quality: 90 })
-    .toFile("./public/gallery/normal/" + fileName);
-  res.render("photoupload");
-  //teeme 2 erisuurust
-  sharp(req.file.destination + fileName)
-    .resize(100, 100)
-    .jpeg({ quality: 90 })
-    .toFile("./public/gallery/thumb/" + fileName);
-  //salvestame andmebaasi
-  let sqlReq =
-    "INSERT INTO photos (file_name, orig_name, alt_text, privacy, user_id) VALUES (?,?,?,?,?)";
+  if (!req.file) {
+    return res.render("photoupload", {
+      message: "Viga: Foto ei ole yleslaaditud!",
+    });
+  }
 
-  const userId = 1;
-  conn.query(sqlReq, [
-    fileName,
-    req.file.originalname,
-    req.body.altInput,
-    req.body.privacyInput,
-    userId,
-    (err, result) => {
+  try {
+    const fileName = "vp_" + Date.now() + ".jpg";
+
+    fs.rename(req.file.path, req.file.destination + fileName, (err) => {
       if (err) {
         throw err;
-      } else {
-        res.render("photoupload");
       }
-    },
-  ]);
+      console.log(err);
+    });
+
+    sharp(req.file.destination + fileName)
+      .resize(800, 600)
+      .jpeg({ quality: 90 })
+      .toFile("./public/gallery/normal/" + fileName);
+    res.render("photoupload");
+    //teeme 2 erisuurust
+    sharp(req.file.destination + fileName)
+      .resize(100, 100)
+      .jpeg({ quality: 90 })
+      .toFile("./public/gallery/thumb/" + fileName);
+    //salvestame andmebaasi
+    let sqlReq =
+      "INSERT INTO photos (file_name, orig_name, alt_text, privacy, user_id) VALUES (?,?,?,?,?)";
+
+    const userId = 1;
+    conn.query(sqlReq, [
+      fileName,
+      req.file.originalname,
+      req.body.altInput,
+      req.body.privacyInput,
+      userId,
+      (err, result) => {
+        if (err) {
+          throw err;
+        } else {
+          res.render("photoupload", {
+            message: "Foto yleslaaditud!",
+          });
+        }
+      },
+    ]);
+  } catch (error) {
+    console.error(error);
+    res.render("photoupload", { message: "Viga foto yleslaadimisel!" });
+  }
+});
+
+// näitab kõike avalikud fotod
+app.get("/publicGallery", (req, res) => {
+  const sqlReq = "SELECT * FROM photos WHERE privacy = 3 ORDER BY id DESC";
+  conn.query(sqlReq, (err, sqlres) => {
+    if (err) {
+      throw err;
+    } else {
+      res.render("publicGallery", { photos: sqlres });
+    }
+  });
+});
+
+app.get("/signup", (req, res) => {
+  res.render("signup");
+});
+
+app.post("/signup", (req, res) => {
+  let notice = "Ootan andmeid";
+
+  const {
+    firstNameInput,
+    lastNameInput,
+    birthDateInput,
+    genderInput,
+    emailInput,
+    passwordInput,
+    confirmPasswordInput,
+  } = req.body;
+
+  if (
+    !firstNameInput ||
+    !lastNameInput ||
+    !birthDateInput ||
+    !genderInput ||
+    !emailInput
+  ) {
+    notice = "Moned andmeid puuduvad.";
+    console.log("Signup failed. err: br1");
+    return res.status(400).render("signup", { notice });
+  }
+
+  if (!passwordInput || !confirmPasswordInput) {
+    notice = "Salasona puudub.";
+    console.log("Signup failed. err: br2");
+    return res.status(400).render("signup", { notice });
+  } else if (passwordInput.length < 8) {
+    notice = "Salasona on liiga luhike";
+    console.log("Signup failed. err: br3");
+    return res.status(400).render("signup", { notice });
+  } else if (passwordInput !== confirmPasswordInput) {
+    notice = "Parooli ei kattu.";
+    console.log("Signup failed. err: br4");
+    return res.status(400).render("signup", { notice });
+  }
+
+  const emailQuery = conn.query(
+    "SELECT * FROM users WHERE email = ?",
+    [emailInput],
+    (err, sql1res) => {
+      if (!err) {
+        if (sql1res.length > 0) {
+          notice = "Kasutaja on juba olemas.";
+          console.log("Signup failed. err: user_exists");
+          return res.status(412).render("signup", { notice });
+        }
+      }
+    }
+  );
+
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) {
+      notice = "ERR 500, proovi uuesti.";
+      console.log("Signup failed. err: br5");
+      console.error(err);
+      return res.status(500).render("signup", { notice });
+    } else {
+      bcrypt.hash(passwordInput, salt, (err, encpwd) => {
+        if (err) {
+          notice = "ERR 500, proovi uuesti.";
+          console.log("Signup failed. err: br6");
+          console.error(err);
+          return res.status(500).render("signup", { notice });
+        } else {
+          conn.execute(
+            "INSERT INTO users (first_name, last_name, email, gender, birth_date, password) VALUES (?, ?, ?, ?, ?, ?)",
+            [
+              firstNameInput,
+              lastNameInput,
+              emailInput,
+              genderInput,
+              birthDateInput,
+              encpwd,
+            ],
+            (err, sqlres) => {
+              if (!err) {
+                notice = `Kasutaja ${emailInput} on edukalt loodud!`;
+                console.log("Signup OK");
+                return res.status(201).render("signup", { notice });
+              } else {
+                notice = "ERR 500, proovi uuesti.";
+                console.log("Signup failed. err: br7");
+                console.error(err);
+                return res.status(500).render("signup", { notice });
+              }
+            }
+          );
+        }
+      });
+    }
+  });
 });
 
 app.listen(5117);
